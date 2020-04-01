005a52bef86189b1c367a1fa93077e9e0b907c32
Ankam Divija <ankamdivija@Ankams-MacBook-Air-910.local>
2020-04-01 12:44:27 +0530

    Added Basic home page

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page')
+    # return HttpResponse('Home page')
+    return render(request,'home.html',{})
 
 def login(request):
     return HttpResponse('Login page')
