a4c81d97374a74d0db2e535ff96b1eb8de0d1578
Ankam Divija <ankamdivija@Ankams-MacBook-Air-910.local>
2020-04-08 01:48:03 +0530

    Added decorators and signals to login/signup

--- PS/views.py
+++ PS/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url = 'login')
 def dashboard(request):
-    return render(request,'app/dashboard.html',{})
+    user = request.user
+    context = {
+        'user':user,
+    }
+    return render(request,'app/dashboard.html',context)
